d0b6cd4da507a2cd5d1d4f1d60501f5abc57b45c
MoveToEx <movetoex@outlook.com>
2020-10-17 20:23:05 +0800

    improve

--- PowershellTagLib.psm1
+++ PowershellTagLib.psm1
@@ -1,49 +1,49 @@
-Function Get-Tag([String]$FileName) {
-    Return [TagLib.File]::Create($FileName).Tag
+Function Get-Tag([String]$Path) {
+    Return [TagLib.File]::Create($Path).Tag
 }
 
-Function Get-Title([String]$FileName) {
-    Return (Get-Tag($FileName)).Title
+Function Get-Title([String]$Path) {
+    Return (Get-Tag($Path)).Title
 }
 
-Function Get-Artist([String]$FileName) {
-    Return (Get-Tag($FileName)).Performers
+Function Get-Artist([String]$Path) {
+    Return (Get-Tag($Path)).Performers
 }
 
-Function Get-Album([String]$FileName) {
-    Return (Get-Tag($FileName)).Album
+Function Get-Album([String]$Path) {
+    Return (Get-Tag($Path)).Album
 }
 
-Function Get-Comment([String]$FileName) {
-    Return (Get-Tag($FileName)).Comment
+Function Get-Comment([String]$Path) {
+    Return (Get-Tag($Path)).Comment
 }
 
-Function Set-Tag([String]$FileName, [String]$Name, $Value) {
-    $Meta = [TagLib.File]::Create($FileName)
+Function Set-Tag([String]$Path, [String]$Name, $Value) {
+    $Meta = [TagLib.File]::Create($Path)
     $Meta[$Name] = $Value
     $Meta.Save()
 }
 
-Function Set-Title([String]$FileName, [String]$Title) {
-    $Meta = [TagLib.File]::Create($FileName)
+Function Set-Title([String]$Path, [String]$Title) {
+    $Meta = [TagLib.File]::Create($Path)
     $Meta.Tag.Title = $Title
     $Meta.Save()
 }
 
-Function Set-Artist([String]$FileName, [String[]]$Artist) {
-    $Meta = [TagLib.File]::Create($FileName)
+Function Set-Artist([String]$Path, [String[]]$Artist) {
+    $Meta = [TagLib.File]::Create($Path)
     $Meta.Tag.AlbumArtists = $Meta.Tag.Performers = $Artist
     $Meta.Save()
 }
 
-Function Set-Album([String]$FileName, [String]$Album) {
-    $Meta = [TagLib.File]::Create($FileName)
+Function Set-Album([String]$Path, [String]$Album) {
+    $Meta = [TagLib.File]::Create($Path)
     $Meta.Tag.Album = $Album
     $Meta.Save()
 }
 
-Function Set-Comment([String]$FileName, [String]$Comment) {
-    $Meta = [TagLib.File]::Create($FileName)
+Function Set-Comment([String]$Path, [String]$Comment) {
+    $Meta = [TagLib.File]::Create($Path)
     $Meta.Tag.Comment = $Comment
     $Meta.Save()
 }
--- README.md
+++ README.md
@@ -1,1 +1,11 @@
 # PowershellTagLib
+
+A module for powershell to read tag of MP3/FLAC files based on TagLibSharp.
+
+# Usage
+
+```powershell
+PS> cd ((ls Env:\PSModulePath).Value -split ';')[0]
+PS> git clone https://github.com/MoveToEx/PowershellTagLib
+PS> Import-Module PowershellTagLib
+```
