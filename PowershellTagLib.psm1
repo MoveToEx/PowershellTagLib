b4c6de043501196d956f664a16d023833ccdeaf6
MoveToEx <movetoex@outlook.com>
2020-10-17 11:22:44 +0800

    improve

--- PowershellTagLib.psm1
+++ PowershellTagLib.psm1
@@ -14,3 +14,20 @@ Function Get-Album([String]$FileName) {
     Return (Get-Tag($FileName)).Album
 }
 
+Function Set-Title([String]$FileName, [String]$Title) {
+    $Meta = [TagLib.File]::Create($FileName)
+    $Meta.Tag.Title = $Title
+    $Meta.Save()
+}
+
+Function Set-Artist([String]$FileName, [String[]]$Artist) {
+    $Meta = [TagLib.File]::Create($FileName)
+    $Meta.Tag.AlbumArtists = $Meta.Tag.Performers = $Artist
+    $Meta.Save()
+}
+
+Function Set-Album([String]$FileName, [String]$Album) {
+    $Meta = [TagLib.File]::Create($FileName)
+    $Meta.Tag.Album = $Album
+    $Meta.Save()
+}
